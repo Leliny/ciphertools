e7ca3aa9e73f40972eaab095b357b9aab7dde0ff
Aakash Patel <aakpat6@gmail.com>
2014-09-25 21:26:21 -0400

    Add tests for Caesar.

---
+++ ciphertools/test_caesar.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from caesar import caesar, caesar_solve
+
+
+class TestCaesar(TestCase):
+  def setUp(self):
+    self.ciphers = [
+      ('hello how are you today?', 'mjqqt mtb fwj dtz ytifd?', 5),
+    ]
+
+  def test_caesar(self):
+    for plain, cipher, shift in self.ciphers:
+      enc = caesar(plain, 5)
+      self.assertEqual(cipher, enc)
+
+  def test_caesar_solve(self):
+    for plain, cipher, _ in self.ciphers:
+      dec = caesar_solve(cipher)
+      self.assertEqual(plain, dec[0])
